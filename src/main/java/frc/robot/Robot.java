1a1114b265a850bc8a6241c35254960db302a4ee
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-01-09 14:00:26 +0000

    Testing Workflows V7

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+//import SwerveDrive.java
+import frc.robot.subsystems.swerve.SwerveDrive;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -16,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+  private SwerveDrive m_swerveDrive = new SwerveDrive();
 
   private RobotContainer m_robotContainer;
 
