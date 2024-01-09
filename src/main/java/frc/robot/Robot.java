51b6e99fca16824ba1a2a6c69006cdb4b82cd3d5
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-01-09 16:48:03 +0000

    Workflows should work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.swerve.SwerveDrive;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,7 +17,7 @@ import frc.robot.subsystems.swerve.SwerveDrive;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-  public static SwerveDrive swerveDrive = new SwerveDrive();
+  
   private RobotContainer m_robotContainer;
 
   /**
