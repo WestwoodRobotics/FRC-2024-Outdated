1853e35c2729d5dd31f2b2546dbd33101662dad2
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-01-09 19:00:50 +0000

    Maybe?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.swerve.*;
 
 
 /**
@@ -18,7 +17,7 @@ import frc.robot.subsystems.swerve.*;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-  private SwerveDrive m_swerveDrive = new SwerveDrive();
+  
   
   private RobotContainer m_robotContainer;
 
