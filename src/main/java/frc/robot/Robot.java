a69b30e6f5d532b8a79f3df7e80dffa272468421
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-01-09 21:20:55 +0000

    Pls don't work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.swerve.*;
 
 
 /**
@@ -17,7 +18,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-  
+  public SwerveDrive swerveDrive = new SwerveDrive();
   
   private RobotContainer m_robotContainer;
 
