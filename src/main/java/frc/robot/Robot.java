c0088be7999d609b5d2a568facb2963e0fb99000
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-01-09 18:53:59 +0000

    Testing Workflows

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.swerve.*;
 
 
 /**
@@ -17,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+  private SwerveDrive m_swerveDrive = new SwerveDrive();
   
   private RobotContainer m_robotContainer;
 
